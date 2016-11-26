b0159ab1935c94811d21aa58a25e17e6b9998d86
vreddhi <vreddhi.bhat@gmail.com>
2016-11-26 13:05:17 +0530

    Delta changes to header.js and html files

--- headers.js
+++ headers.js
@@ -1,19 +1,15 @@
-      chrome.webRequest.onBeforeSendHeaders.addListener(function(request) {
-      	request.requestHeaders.push({name: "Vreddhi", value: "test"});
-      	return {requestHeaders: request.requestHeaders};
-      },
-      {urls: ["*://*/*"]},
-      ["requestHeaders","blocking"]);
-
-      chrome.webRequest.onHeadersReceived.addListener(function(details) { 
-      	console.log("**********--------------********************" );
-      	console.log("URL " + details.url);
-	    details.responseHeaders.forEach(function(v){	    	      
-		      if(v.name == "X-Cache" || v.name == "Date" || v.name == "X-Akamai-Request-ID"){
-		       console.log("Name: " + v.name + " Value: " + v.value );		 		        
-		      }
-	      })			  		       	
-         },
-        {urls: ["*://*/*"]},
-        ["responseHeaders"]);
+chrome.webRequest.onHeadersReceived.addListener(function(details) { 
+  console.log("**********--------------********************" );
+  console.log("URL " + details.url);
+  var requests = {};
+    details.responseHeaders.forEach(function(v){              
+      if(v.name == "X-Cache" || v.name == "Date" || v.name == "X-Akamai-Request-ID"){
+       console.log("Name: " + v.name + " Value: " + v.value );     
+      }
+        var div = document.createElement("div");
+        div.textContent = "Name";
+    })                    
+   },
+  {urls: ["*://www.ndtv.com/*"]},
+  ["responseHeaders"]);
 
